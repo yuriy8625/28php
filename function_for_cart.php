9c630d4d2317fd9096610c130fd7d5bd1c8228ef
Yuriy Kernytskyi <ykernytskyi@gmail.com>
2018-11-09 22:38:15 +0200

    add ver.infinite

--- function_for_cart.php
+++ function_for_cart.php
@@ -1,7 +1,6 @@
 <?php
+
 $bag = [];
-
-
 
 // Добавить в корзину
 	function add($arr, $id, $quantity, $price){
@@ -11,40 +10,36 @@ $bag = [];
 		$arr['total amount'] = 0;
 
 		$arr['items'][] = ['id'=>$id, 'quantity'=>$quantity, 'price'=>$price];
-		
-			
-		// считает общуюю сумму и количество
-		for($i = 0; $i < count($arr['items']); $i++){
-			$arr['sum'] += $arr['items'][$i]['price'];
-			$arr['total amount'] += $arr['items'][$i]['quantity'];
-		}
 
 		// считает сумму с учетом скидки
-		if($arr['total amount'] < 10 && $arr['sum'] > 2000){
-			$arr['sum'] *= 0.93;
-		}elseif ($quantity > 10) {
-			$arr['sum'] *= 0.9;
-		}				
+			$arr =	discont($arr);	
 	
 		return $arr;
 	}
 	
-// $bag = add($bag, 1, 1,1);
-// $bag = add($bag, 2, 1,1);
-// $bag = add($bag, 3, 1,1);
+// $bag = add($bag, 1, 10,100);
+// $bag = add($bag, 2, 10,100);
+// $bag = add($bag, 3, 9,100);
 // var_dump($bag);
 
 // Удалить с корзины
 	function remove($arr, $id){
 
 		for($i = 0; $i < count($arr['items']); $i++){
+
 			if($arr['items'][$i]['id'] == $id){
 				unset($arr['items'][$i]);
 			}
-		}
+
+	   	}  
+	   	
+		// считает сумму с учетом скидки
+			$arr = discont($arr);	
+
 		return $arr;
 	}
-// $bag = remove($bag,2);
+
+// $bag = remove($bag, 2);
 // var_dump($bag);
 
 // Изменить количество
@@ -56,23 +51,36 @@ function quantity($arr, $id, $n){
 			$arr['items'][$i]['quantity'] = $n;
 		}
 	}
+
+		// считает сумму с учетом скидки
+			$arr =	discont($arr);	
+		
 	return $arr;
 }
 
-// $bag = quantity($bag,1, 44599084);
-// var_dump($bag);
+$bag = quantity($bag, 1, 445);
+var_dump($bag);
 
 // считает сумму с учетом скидки
-	function discont($sum,$quantity){
+	function discont($arr){
 
-		if($quantity < 10 && $sum > 2000){
-			$sum *= 0.93;
-		}elseif ($quantity > 10) {
-			$sum *= 0.9;
+		$arr['sum'] = 0;
+		$arr['total amount'] = 0;
+
+		// считает общуюю сумму и количество
+		for($i = 0; $i < count($arr['items']); $i++){
+			$arr['sum'] += $arr['items'][$i]['price'];
+			$arr['total amount'] += $arr['items'][$i]['quantity'];
 		}
 
-		return $sum;
-	}
-?>
+		// Выбирает какую сделать скидку
+		if($arr['total amount'] < 10 && $arr['sum'] > 2000){
+			 $arr['sum'] *= 0.93;
+		}elseif ($arr['total amount'] > 10) {
+			 $arr['sum'] *= 0.9;
+		}
 
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+		return  $arr;
+	}
+	
+?>
